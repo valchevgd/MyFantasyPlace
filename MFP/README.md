e81c345f52ca4197e426825c835262387d1d35da
Georgi Valchev <valchevgd@gmail.com>
2018-12-18 21:27:31 +0200

    Start

---
+++ MFP/README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on December 18, 2018, 5:46 pm.
---
+++ MFP/src/MyFantasyPlaceBundle/MyFantasyPlaceBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MyFantasyPlaceBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MyFantasyPlaceBundle extends Bundle
+{
+}
